af9ebb629e31341be431be751ccff832cc00c8d2
Ariel <arielord@buffalo.edu>
2018-11-27 18:36:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  fizz_or_buzz = ""
+  if (num/3 == 0) && (num/5 == 0)
+    fizz_or_buzz = "FizzBuzz"
+    
+  if num/3 == 0
+    fizz_or_buzz = "Fizz"
+  elsif num/5 == 0
+    fizz_or_buzz = "Buzz"
+  end
+end
